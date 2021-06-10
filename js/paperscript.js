a002964bf8a695ef84b70e4998bdb24ee6563906
Matej-Sulfrian <matej.sulfrian@hs-furtwangen.de>
2021-06-10 16:46:05 +0200

    second try level creation

--- js/paperscript.js
+++ js/paperscript.js
@@ -1,4 +1,4 @@
-var Ball = function(point, vector) {
+var Ball = function (point, vector) {
     this.vector = vector;
     this.point = point;
     this.gravity = 0.9;
@@ -30,66 +30,109 @@ var Ball = function(point, vector) {
     });
 }
 
-var Plank = function(point) {
+var Plank = function (point) {
     this.point = point;
     this.vector = new Point(0, 0)
+    this.gravity = new Point(0, 0.2)
     var color = {
         hue: Math.random() * 360,
         saturation: 1,
         brightness: 1
     };
-    var plank = new Rectangle(this.point, new Point(this.point.x + 150, this.point.y + 20))
-    var radius = new Size (20, 20)
-    var path = new Path.Rectangle(plank, radius)
-    path.fillColor = new Color(color)
+    var radius = this.size = new Size(20, 20)
+    var plank = new CompoundPath({
+        children: [
+            new Path.Rectangle({
+                rectangle: new Rectangle(this.point, new Point(this.point.x + 150, this.point.y + 20)),
+                radius: radius
+            }),
+        ],
+        fillColor: new Color(color)
+    })
+
+    // var path = new Path.Rectangle(plank, radius)
+    // path.fillColor = new Color(color)
+
+    this.item = new Group({
+        children: [plank],
+        applyMatrix: false,
+        position: this.point
+    })
+}
+
+var planks = [];
+
+Plank.prototype.move = function () {
+    var size = view.size
+    this.vector += this.gravity
+
+    if (this.item.position.y >= 700) {
+        console.log("out")
+        this.item.remove()
+        planks.shift()
+        var newOffset = 700 - planks[planks.length - 1].point.y
+        createPlanks(1, newOffset)
+    }
+
+    var max = Point.max(0, this.point + this.vector);
+    this.item.position = this.point = Point.min(max, size - 0);
+
 }
 
 var move = "";
 var left = 0;
 var right = 0;
 
+var movePlanks = false;
+
 function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-var planks = [];
-function createPlanks () {
+function createPlanks(_amount, _offset) {
     var offset = 0;
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < _amount; i++) {
         var y
-        if (i === 0) {
-            y = randomIntFromInterval(500, 700)
+        if (i === 0 && !_offset) {
+            y = randomIntFromInterval(620, 700)
         } else {
-            y = randomIntFromInterval(offset - 70, offset - 200)
+            if (_offset) {
+                offset = _offset
+            }
+            y = randomIntFromInterval(offset - 70, offset - 150)
         }
         offset = y
-        console.log(offset)
         var x = randomIntFromInterval(0, 300)
-        var position = new Point(x , y),
+        var position = new Point(x, y),
             plank = new Plank(position)
         planks.push(plank);
 
     }
 }
-createPlanks()
+
+createPlanks(5, null)
 
 for (var i = 0; i < planks.length; i++) {
     console.log(planks[i])
 }
 
 
-Ball.prototype.iterate = function() {
+Ball.prototype.iterate = function () {
     var size = view.size;
     this.vector.y += this.gravity;
     var pre = this.point + this.vector;
 
     if (pre.y < this.radius || pre.y > size.height - this.radius) {
+        console.log(this.vector.y)
         this.vector.y = this.bounce;
-
     } else {
         for (var i = 0; i < planks.length; i++) {
-            if (pre.y < planks[i].point.y && pre.y > planks[i].point.y - 40 && pre.x > planks[i].point.x && pre.x < planks[i].point.x + 150) {
+            if (pre.y < planks[i].point.y && pre.y > planks[i].point.y - 40 && pre.x > planks[i].point.x && pre.x < planks[i].point.x + 150 && this.vector.y > 8) {
                 this.vector.y = this.bounce;
+                // movePlanks = true
+                for (var p = 0; p < planks.length; p++) {
+                    planks[p].move()
+                }
                 break
             }
         }
@@ -117,7 +160,8 @@ Ball.prototype.iterate = function() {
 
 
 var balls = [];
-function createBalls () {
+
+function createBalls() {
     for (var i = 0; i < 1; i++) {
         var position = new Point(50, 50) * view.size,
             vector = new Point(0, 0),
@@ -125,6 +169,7 @@ function createBalls () {
         balls.push(ball);
     }
 }
+
 createBalls()
 
 // var textItem = new PointText({
@@ -135,8 +180,15 @@ createBalls()
 
 
 function onFrame() {
-    for (var i = 0, l = balls.length; i < l; i++)
+    for (var i = 0, l = balls.length; i < l; i++) {
         balls[i].iterate();
+    }
+    if (movePlanks === true) {
+        for (var p = 0; p < planks.length; p++) {
+            planks[p].move()
+        }
+    }
+
 }
 
 tool.onKeyDown = function (eventObject) {
