0291e1a1fd1c3dc3d8ecefbba10556429010409b
Matej-Sulfrian <matej.sulfrian@hs-furtwangen.de>
2021-12-16 17:37:17 +0100

    active plate logic

--- interface_design/interfaceDesign.js
+++ interface_design/interfaceDesign.js
@@ -1,10 +1,51 @@
+let plates,
+    mid
+
 $('html').ready(function () {
     $('.showHideW').css('display', 'none')
     console.log('load')
+
+    plates = [
+        {
+            touchPlate: $('.touch_w .mid .top .touch_herd[data="0"]'),
+            midActive: false,
+            state: 0,
+            plate: $('.cook .top .plate[data="0"]')
+        },
+        {
+            touchPlate: $('.touch_w .mid .top .touch_herd[data="1"]'),
+            midActive: false,
+            state: 0,
+            plate: $('.cook .top .plate[data="1"]')
+        },
+        {
+            touchPlate: $('.touch_w .mid .bottom .touch_herd[data="2"]'),
+            midActive: false,
+            state: 0,
+            plate: $('.cook .bottom .plate[data="2"]')
+        },
+        {
+            touchPlate: $('.touch_w .mid .bottom .touch_herd[data="3"]'),
+            midActive: false,
+            state: 0,
+            plate: $('.cook .bottom .plate[data="3"]')
+        }
+    ]
+
+    mid = [
+        {
+            el: $('.touch_w .mid .top img'),
+            active: false
+        },
+        {
+            el: $('.touch_w .mid .bottom img'),
+            active: false
+        }
+    ]
+
 })
 
-
-function onOff () {
+function onOff() {
 
     let onOffButton = $('.touch_w .left img')
 
@@ -31,7 +72,73 @@ function onOff () {
         }, 300)
 
     }
+}
 
+function switchPlate(plateI) {
+    for (let i = 0; i < plates.length; i++) {
+        if (i !== plateI) {
+            plates[i].touchPlate.removeClass('active')
+        }
+    }
 
+    if (plates[plateI].midActive) {
+        let x
+        if (plateI < 2) {
+            x = 0
+        } else {
+            x = 2
+        }
+        if (plates[plateI].touchPlate.hasClass('active')) {
+            for (let i = 0 + x; i < 2 + x; i++) {
+                plates[i].touchPlate.removeClass('active')
+            }
+        } else {
+            for (let i = 0 + x; i < 2 + x; i++) {
+                plates[i].touchPlate.addClass('active')
+            }
+        }
+    } else {
+        if (plates[plateI].touchPlate.hasClass('active')) {
+            plates[plateI].touchPlate.removeClass('active')
+        } else {
+            plates[plateI].touchPlate.addClass('active')
+        }
+    }
+}
+
+function switchMid(midI) {
+    let x
+    if (midI === 0) {
+        x = 0
+    } else {
+        x = 2
+    }
+
+    if (!mid[midI].active) {
+        mid[midI].el.attr('src', 'interface_design/mid_sec_red.svg')
+        mid[midI].active = true
+
+        for (let plate of plates) {
+            plate.touchPlate.removeClass('active')
+        }
+
+        for (let i = 0 + x; i < 2 + x; i++) {
+            let plate = plates[i].touchPlate
+            plates[i].midActive = true
+            if (!plate.hasClass('active')) {
+                plate.addClass('active')
+            }
+        }
+
+    } else {
+        mid[midI].el.attr('src', 'interface_design/mid_sec.svg')
+        mid[midI].active = false
+
+        for (let i = 0 + x; i < 2 + x; i++) {
+            let plate = plates[i].touchPlate
+            plates[i].midActive = false
+            plate.removeClass('active')
+        }
+    }
 }
 
